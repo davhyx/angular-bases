99d161c2049c403b814d2f2e10e311944edcaec9
davhyx <piensoencodigo@gmail.com>
2025-03-12 09:51:44 +0100

    pipes app - añadimos locale 'es'

--- 04_pipes-app/src/app/app.config.ts
+++ 04_pipes-app/src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs, 'es');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ]
 };
