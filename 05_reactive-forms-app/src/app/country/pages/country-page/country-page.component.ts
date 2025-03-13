92f1d3e2e05dacfe5ce4baddbc8face890c26ba8
davhyx <piensoencodigo@gmail.com>
2025-03-13 14:59:48 +0100

    forms-app - reactive form, form builder, validations, dinamic form

---
+++ 05_reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-page',
+  imports: [],
+  templateUrl: './country-page.component.html',
+  standalone: true
+})
+export class CountryPageComponent { }
